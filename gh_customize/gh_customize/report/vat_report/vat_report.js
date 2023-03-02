8c866e42cb68137ae1b1b5451acab3a44f0b55fb
carbonitesolutions <admin@carbonitesolutions.com>
2023-03-02 17:23:33 +0000

    This VAT Report has a filter for user default company, status as submitted, from and to dates and rates filter. The data being fetecd is first the Sales VAT and the Purchase VAT. There are three totals after this data is generated, first being the sales totals, followed by purchase and then the VAT to be paid.

---
+++ gh_customize/gh_customize/report/vat_report/vat_report.js
@@ -0,0 +1,43 @@
+// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["VAT Report"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"read_only": 1,
+		},
+		{
+			"fieldname":"submitted",
+			"label": __("Status"),
+			"fieldtype": "Data",
+			"default": "Submitted",
+			"read_only": 1,
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "100px",
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "100px",
+		},
+		{
+			"fieldname":"rates",
+			"label": __("Rates"),
+			"fieldtype": "Data",
+			"width": "100px",
+		}
+
+
+	]
+};
